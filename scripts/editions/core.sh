e3a6bd5f6b57cf35709471e50a24f9821d409456
Apostolos Chalis <tolishalis@gmail.com>
2025-12-23 23:41:44 +0200

    Direct DEB download of DBeaver

--- scripts/editions/core.sh
+++ scripts/editions/core.sh
@@ -10,8 +10,9 @@ core() {
     run "wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg" "Adding VSCodium GPG key"
     run "echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' | tee /etc/apt/sources.list.d/vscodium.list" "Adding VSCodium repository"
 
-    run "wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | gpg --dearmor -o /usr/share/keyrings/dbeaver-keyring.gpg" "Adding DBeaver GPG key"
-    run "echo 'deb [signed-by=/usr/share/keyrings/dbeaver-keyring.gpg] https://dbeaver.io/debs/dbeaver-ce /' | tee /etc/apt/sources.list.d/dbeaver.list" "Adding DBeaver repository"
+    # direct download DBeaver DEB package because the repository is not reliable
+    run "wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb -O /tmp/dbeaver-ce.deb" "Downloading DBeaver DEB package"
+    run "apt install -y /tmp/dbeaver-ce.deb" "Installing DBeaver from DEB"
 
     run "apt update" "Updating package lists"
     install_packages "${core_packages[@]}"
