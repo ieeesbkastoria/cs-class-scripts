36d402b65cc4054b15375a84e446e4314a018d23
Apostolos Chalis <tolishalis@gmail.com>
2025-12-23 23:31:08 +0200

    Added repositories for certain pieces of software in core.sh

--- scripts/editions/core.sh
+++ scripts/editions/core.sh
@@ -7,9 +7,14 @@ core() {
     local core_packages
     mapfile -t core_packages < config/packages/core.txt
 
+    run "wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg" "Adding VSCodium GPG key"
+    run "echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' | tee /etc/apt/sources.list.d/vscodium.list" "Adding VSCodium repository"
+
+    run "wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | gpg --dearmor -o /usr/share/keyrings/dbeaver-keyring.gpg" "Adding DBeaver GPG key"
+    run "echo 'deb [signed-by=/usr/share/keyrings/dbeaver-keyring.gpg] https://dbeaver.io/debs/dbeaver-ce /' | tee /etc/apt/sources.list.d/dbeaver.list" "Adding DBeaver repository"
+
     run "apt update" "Updating package lists"
     install_packages "${core_packages[@]}"
-    # run "wget -qO- https://deb.parrotsec.org/parrot/misc/parrotsec.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/parrot-archive-keyring.gpg" "Adding GPG key"
-    run "apt update" "Updating package lists"
+
     run "apt upgrade -y" "Upgrading packages"
 }
